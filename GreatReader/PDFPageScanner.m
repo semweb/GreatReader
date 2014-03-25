49daed32db26674a2739bc12ef32eb1ac9293880
Miyamoto Shohei <semwebmail@gmail.com>
2014-03-25 13:23:10 +0900

    Log!!!

--- GreatReader/PDFPageScanner.m
+++ GreatReader/PDFPageScanner.m
@@ -253,8 +253,6 @@ void ConcatMatrix(CGPDFScannerRef scanner, void *info)
 
 - (void)didFindString:(NSString *)string state:(PDFTextState *)state
 {
-    NSLog(@"%@", string);
-    
     for (int i = 0; i < string.length; i++) {
         unichar c = [string characterAtIndex:i];
         PDFRenderingCharacter *character =
