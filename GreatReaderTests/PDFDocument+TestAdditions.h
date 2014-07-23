10131d7f0aed3c04d6e0e5b3a9f1a9d6d55823e5
Miyamoto Shohei <semwebmail@gmail.com>
2014-07-23 21:58:56 +0900

    Add slider model tests

---
+++ GreatReaderTests/PDFDocument+TestAdditions.h
@@ -0,0 +1,13 @@
+//
+//  PDFDocument+TestAdditions.h
+//  GreatReader
+//
+//  Created by MIYAMOTO Shohei on 7/23/14.
+//  Copyright (c) 2014 MIYAMOTO Shohei. All rights reserved.
+//
+
+#import "PDFDocument.h"
+
+@interface PDFDocument (TestAdditions)
++ (PDFDocument *)test_documentWithPDFName:(NSString *)name;
+@end
