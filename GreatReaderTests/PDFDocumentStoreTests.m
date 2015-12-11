c3b131286c07be469516fc087fda6bc265fcaedb
antonc27 <antonc27@mail.ru>
2015-12-11 02:22:03 +0100

    - More UTs for moving of PDF documents to folder for PDFDocumentStore

--- GreatReaderTests/PDFDocumentStoreTests.m
+++ GreatReaderTests/PDFDocumentStoreTests.m
@@ -16,6 +16,7 @@
 
 @interface PDFDocumentStoreTests : XCTestCase
 @property (nonatomic, strong) PDFDocumentStore *pdfDocumentStore;
+@property (nonatomic, strong) Folder *fakeFolder;
 @end
 
 @implementation PDFDocumentStoreTests
@@ -27,13 +28,39 @@
     [super setUp];
     
     self.pdfDocumentStore = [[PDFDocumentStore alloc] init];
+    self.fakeFolder = [[Folder alloc] initWithPath:@"fake_folder_path"];
 }
 
 - (void)tearDown
 {
+    self.fakeFolder = nil;
     self.pdfDocumentStore = nil;
     
     [super tearDown];
+}
+
+#pragma mark - Helpers
+
+- (void)stubMoveToDirectoryCallForPDFDocumentMock:(PDFDocument *)pdfDocumentMock
+{
+    [self stubMoveToDirectoryCallForPDFDocumentMock:pdfDocumentMock
+                                   andReturnSuccess:YES];
+}
+
+- (void)stubMoveToDirectoryCallForPDFDocumentMock:(PDFDocument *)pdfDocumentMock andReturnSuccess:(BOOL)success
+{
+    OCMStub([pdfDocumentMock moveToDirectory:[OCMArg any] error:((NSError *__autoreleasing *)[OCMArg anyPointer])]).andReturn(success);
+}
+
+- (void)rejectMoveToDirectoryCallForPDFDocumentMock:(PDFDocument *)pdfDocumentMock
+{
+    // as OCMock doesnt have good implementation of 'reject', just throw uncaught exception for moveToDirectory:error: method
+    OCMStub([pdfDocumentMock moveToDirectory:[OCMArg any] error:((NSError *__autoreleasing *)[OCMArg anyPointer])]).andThrow([NSException new]);
+}
+
+- (void)verifyMoveToDirectoryCallForPDFDocumentMock:(PDFDocument *)pdfDocumentMock
+{
+    OCMVerify([pdfDocumentMock moveToDirectory:[OCMArg any] error:((NSError *__autoreleasing *)[OCMArg anyPointer])]);
 }
 
 #pragma mark - Tests
@@ -43,14 +70,47 @@
     PDFDocument *fakePdfDocument = [[PDFDocument alloc] initWithPath:@"fake_pdf_path"];
     id fakePdfDocumentMock = OCMPartialMock(fakePdfDocument);
     
-    Folder *fakeFolder = [[Folder alloc] initWithPath:@"fake_folder_path"];
-    
-    OCMStub([fakePdfDocumentMock moveToDirectory:[OCMArg any] error:((NSError *__autoreleasing *)[OCMArg anyPointer])]).andReturn(YES);
+    [self stubMoveToDirectoryCallForPDFDocumentMock:fakePdfDocumentMock];
     
     NSError *error = nil;
-    [self.pdfDocumentStore moveDocument:fakePdfDocument toFolder:fakeFolder error:&error];
+    [self.pdfDocumentStore moveDocument:fakePdfDocument toFolder:self.fakeFolder error:&error];
     
-    OCMVerify([fakePdfDocumentMock moveToDirectory:[OCMArg any] error:((NSError *__autoreleasing *)[OCMArg anyPointer])]);
+    [self verifyMoveToDirectoryCallForPDFDocumentMock:fakePdfDocumentMock];
+}
+
+- (void)testMoveToDirectoryCalledForTwoDocuments
+{
+    PDFDocument *fakePdfDocumentOne = [[PDFDocument alloc] initWithPath:@"fake_pdf_path_1"];
+    id fakePdfDocumentOneMock = OCMPartialMock(fakePdfDocumentOne);
+    
+    PDFDocument *fakePdfDocumentTwo = [[PDFDocument alloc] initWithPath:@"fake_pdf_path_2"];
+    id fakePdfDocumentTwoMock = OCMPartialMock(fakePdfDocumentTwo);
+    
+    [self stubMoveToDirectoryCallForPDFDocumentMock:fakePdfDocumentOneMock];
+    [self stubMoveToDirectoryCallForPDFDocumentMock:fakePdfDocumentTwoMock];
+    
+    NSError *error = nil;
+    NSArray *documents = @[fakePdfDocumentOne, fakePdfDocumentTwo];
+    [self.pdfDocumentStore moveDocuments:documents toFolder:self.fakeFolder error:&error];
+    
+    [self verifyMoveToDirectoryCallForPDFDocumentMock:fakePdfDocumentOneMock];
+    [self verifyMoveToDirectoryCallForPDFDocumentMock:fakePdfDocumentTwoMock];
+}
+
+- (void)testStoppingOfMovingIfOneDocumentFailedToMove
+{
+    PDFDocument *fakePdfDocumentOne = [[PDFDocument alloc] initWithPath:@"fake_pdf_path_1"];
+    id fakePdfDocumentOneMock = OCMPartialMock(fakePdfDocumentOne);
+    
+    PDFDocument *fakePdfDocumentTwo = [[PDFDocument alloc] initWithPath:@"fake_pdf_path_2"];
+    id fakePdfDocumentTwoMock = OCMPartialMock(fakePdfDocumentTwo);
+    
+    [self stubMoveToDirectoryCallForPDFDocumentMock:fakePdfDocumentOneMock andReturnSuccess:NO];
+    [self rejectMoveToDirectoryCallForPDFDocumentMock:fakePdfDocumentTwoMock];
+    
+    NSError *error = nil;
+    NSArray *documents = @[fakePdfDocumentOne, fakePdfDocumentTwo];
+    XCTAssertFalse([self.pdfDocumentStore moveDocuments:documents toFolder:self.fakeFolder error:&error], @"Method must return NO if moving failed");
 }
 
 @end
